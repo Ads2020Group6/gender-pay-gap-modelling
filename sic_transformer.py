0bd5494ac19f12cc2eb4742c14341d2f9ded8650
Leandro Guardia <lean.guardia03@gmail.com>
2020-05-13 01:26:17 +0100

    Building dummies box of zeroes

--- sic_transformer.py
+++ sic_transformer.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def codes_to(df, typ):
     df.SicCodes = df.SicCodes.astype(typ)
@@ -26,6 +27,21 @@ def load_codes():
     codes = codes_to(codes, int)
     return codes
 
+# def get_codes_dict():
+#     codes = load_codes()
+#     code_to_section = {}
+#     for i, sic_code in enumerate(codes.SicCodes):
+#         row = codes.iloc[i]
+#         code_to_section[row.SicCodes] = row.Section
+#     return code_to_section
+
+def get_unique_sections():
+    return pd.unique(load_codes().Section)
+
+def build_empty_dummies(df, sections):
+    zeroes = np.zeros((df.shape[0], len(sections)))
+    return pd.DataFrame(zeroes, columns=sections)
+
 def explode_sectors(df, save_file=False, output_filename='data/ukgov-gpg-full-sectors.csv'):
     df = codes_to(df, str)
     df = parse_codes(df)
@@ -40,9 +56,23 @@ def explode_sectors(df, save_file=False, output_filename='data/ukgov-gpg-full-se
     if save_file: df.to_csv(output_filename, index=False)
     return df
 
+def split_sectors(df):
+    df = codes_to(df, str)
+    df = parse_codes(df)
+    sections = get_unique_sections()
+    print(sections)
+    dummies = build_empty_dummies(df, sections)
+    print(dummies.shape)
+    # df = pd.merge(df, load_codes(), on=['SicCodes'])
+    # for i, sic_code in enumerate(df.SicCodes):
+    #     indices = dummies.columns.get_indexer()
+    # df = encode_missing_values(df)
+    return df
+
 def main():
     df = pd.read_csv('data/ukgov-gpg-full.csv')
-    df = explode_sectors(df, save_file=True)
+    # df = explode_sectors(df, save_file=True)
+    return split_sectors(df)
 
 if __name__ == "__main__":
-    main()
+    df = main()
